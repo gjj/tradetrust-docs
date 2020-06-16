a95476e882c7b5221fb7d1339762337424e47e48
Goi Jia Jian <jiajiannn@gmail.com>
2020-06-16 19:11:52 +0800

    docs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const oa = require("@govtechsg/open-attestation");
 
-document = fs.readFileSync("./docs/sample-dns-verified-special-characters.json", "utf8");
-unwrappedDocument = oa.getData(document);
-fs.writeFileSync("./docs/sample-dns-verified-special-characters-unwrapped.json", unwrappedDocument);
+document = fs.readFileSync("./docs/sample-dns-verified-special-characters-unwrapped.json", "utf8");
+wrappedDocument = oa.wrapDocument(document);
+fs.writeFileSync("./docs/sample-dns-verified-special-characters-unwrapped2.json", wrappedDocument);
 
